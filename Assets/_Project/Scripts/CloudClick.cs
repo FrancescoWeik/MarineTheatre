322091fa154a8ef34b1cfb8a19bc45f43a55e134
FrancescoWeikmann <fweikmann@gmail.com>
2026-04-12 17:41:03 +0200

    add cloud click

---
+++ Assets/_Project/Scripts/CloudClick.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CloudClick : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject cloud;
+    [SerializeField] private float destructionGetBigTime = 0.2f;
+    [SerializeField] private float cloudDestructionTime = 0.4f;
+    [SerializeField] private float waitTime = 2f;
+    [SerializeField] private float cloudReAppearTime = 2f;
+    [SerializeField] private float sizeMultiplier = 1.5f;
+    private bool animating = false;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!animating)
+        {
+            StartCoroutine(CloudPop());
+        }
+    }
+
+    private IEnumerator CloudPop()
+    {
+        animating = true;   
+        float elapsed = 0f;
+        Vector3 startScale = cloud.transform.localScale;
+        Vector3 endScale = startScale * sizeMultiplier;
+
+        while (elapsed < destructionGetBigTime)
+        {
+            elapsed = elapsed + Time.deltaTime;
+            cloud.transform.localScale = Vector3.Lerp(startScale, endScale, elapsed / destructionGetBigTime);
+            yield return null;
+        }
+        cloud.transform.localScale = endScale;
+
+        elapsed = 0f;
+        startScale = endScale;
+        endScale = Vector3.zero;
+        //Scale to 0
+        while (elapsed < cloudDestructionTime)
+        {
+            elapsed = elapsed + Time.deltaTime;
+            cloud.transform.localScale = Vector3.Lerp(startScale, endScale, elapsed / cloudDestructionTime);
+            yield return null;
+        }
+        cloud.transform.localScale = endScale;
+
+        yield return new WaitForSeconds(waitTime);
+
+        //Make cloud reAppear
+        elapsed = 0f;
+        startScale = Vector3.one * sizeMultiplier;
+        while (elapsed < cloudReAppearTime)
+        {
+            elapsed = elapsed + Time.deltaTime;
+            cloud.transform.localScale = Vector3.Lerp(endScale, startScale, elapsed / cloudReAppearTime);
+            yield return null;
+        }
+        cloud.transform.localScale = startScale;
+
+        //Make cloud reAppear
+        elapsed = 0f;
+        endScale = Vector3.one;
+        while (elapsed < destructionGetBigTime)
+        {
+            elapsed = elapsed + Time.deltaTime;
+            cloud.transform.localScale = Vector3.Lerp(startScale, endScale, elapsed / destructionGetBigTime);
+            yield return null;
+        }
+        cloud.transform.localScale = endScale;
+
+        animating = false;
+    }
+}
