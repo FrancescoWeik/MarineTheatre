20fa128976d8e8d01e596947a331b14597b97756
FrancescoWeikmann <fweikmann@gmail.com>
2026-07-03 17:52:01 +0200

    Fixed fishDetails and images

--- Assets/_Project/Scripts/Bestiary/FishUIDetails.cs
+++ Assets/_Project/Scripts/Bestiary/FishUIDetails.cs
@@ -4,8 +4,13 @@ using UnityEngine.UI;
 public class FishUIDetails : MonoBehaviour
 {
     [SerializeField] private Image fishImage;
+    [SerializeField] private Image stickImage;
 
     public void SetFishData(FishGeneralDataScriptable fishData) {
         fishImage.sprite = fishData.inPlaySprite;
+        RectTransform rectTransform = stickImage.GetComponent<RectTransform>();
+
+        // Update the anchoredPosition using its current X and your new Y offset
+        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, fishData.stickYOffset);
     }
 }
--- Assets/_Project/Scripts/FishGeneralDataScriptable.cs
+++ Assets/_Project/Scripts/FishGeneralDataScriptable.cs
@@ -5,6 +5,7 @@ public class FishGeneralDataScriptable : ScriptableObject
 {
     public Sprite inPlaySprite;
     public Sprite marketSprite;
+    public float stickYOffset = -288f;
     public string description;
     public string title;
 }
